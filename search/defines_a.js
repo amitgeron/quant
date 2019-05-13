9ed876f8e58a695081906455ebad837c0999121a
Travis CI <travis@travis-ci.org>
2019-05-13 04:37:18 +0000

    Update doxygen documentation on branch gh-pages.
    Travis CI build 1041.
    
    Commit: 1e194f86cdec2dcac0b2c1e72fe4b81762e3c107.

--- search/defines_a.js
+++ search/defines_a.js
@@ -3,7 +3,7 @@ var searchData=
   ['kgranularity',['kGranularity',['../quic_8h.html#a9a5858cdd4682999fab57dbf6db748b1',1,'quic.h']]],
   ['kinitialrtt',['kInitialRtt',['../quic_8h.html#af47b8eb7e21dfad9602ccaa933ff97dd',1,'quic.h']]],
   ['kinitialwindow',['kInitialWindow',['../quic_8h.html#a5abc840ae2172a7f6ad89f37801a39f8',1,'quic.h']]],
-  ['klib_5funused',['klib_unused',['../conn_8c.html#a006a6d732d649728796cfe027e3041d9',1,'klib_unused():&#160;conn.c'],['../pn_8c.html#a006a6d732d649728796cfe027e3041d9',1,'klib_unused():&#160;pn.c'],['../quic_8c.html#a006a6d732d649728796cfe027e3041d9',1,'klib_unused():&#160;quic.c'],['../stream_8c.html#a006a6d732d649728796cfe027e3041d9',1,'klib_unused():&#160;stream.c']]],
+  ['klib_5funused',['klib_unused',['../conn_8h.html#a006a6d732d649728796cfe027e3041d9',1,'klib_unused():&#160;conn.h'],['../pn_8c.html#a006a6d732d649728796cfe027e3041d9',1,'klib_unused():&#160;pn.c'],['../quic_8c.html#a006a6d732d649728796cfe027e3041d9',1,'klib_unused():&#160;quic.c'],['../recovery_8c.html#a006a6d732d649728796cfe027e3041d9',1,'klib_unused():&#160;recovery.c'],['../stream_8c.html#a006a6d732d649728796cfe027e3041d9',1,'klib_unused():&#160;stream.c']]],
   ['klossreductiondivisor',['kLossReductionDivisor',['../quic_8h.html#a89aaa7cfe3dfcee69830f4e5d39201fe',1,'quic.h']]],
   ['kmaxdatagramsize',['kMaxDatagramSize',['../quic_8h.html#aebba1773417b74b712e12052021ac07a',1,'quic.h']]],
   ['kminimumwindow',['kMinimumWindow',['../quic_8h.html#a95be9e468ae520ae408ab00fdcf90a76',1,'quic.h']]],
